bdd0d4852611950a4c22af482f0c439f10e76fdc
Sakhe12 <sakhemzolisa@gmail.com>
2022-11-23 16:31:57 +0200

    update

--- js/code.js
+++ js/code.js
@@ -4,6 +4,20 @@ let outputValue = document.querySelector('#equal')
 // let btnEqual = document.querySelector(eval)
 
 // btnEqual.addEventListener('click', function()){}
+let var = [];
+let btnComma = document.querySelector('#dot');
+btnComma.addEventListener('click', function() {
+    if (!Array.includes('.')) {
+        Array.push('.')
+    }
+})
+let oper = document.querySelectorAll(.operator);
+operators.forEach(operator => {
+    screen.value += operator.textContent,
+    arr = [];
+})
+
+
 function display(number) {
     InputValue.value = InputValue.value + number
 }
