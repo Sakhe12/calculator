a2ea754a661a6aafeff55e6e36a85e86ff6cdedc
Sakhe12 <sakhemzolisa@gmail.com>
2022-11-25 09:55:27 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,13 +1,24 @@
 let InputValue = document.querySelector("#input")
 let outputValue = document.querySelector('#equal')
+let allowComa = true;
 // let display = document.querySelector('.display')
 // let btnEqual = document.querySelector(eval)
 
 // btnEqual.addEventListener('click', function()){}
-
-
 function display(number) {
     InputValue.value = InputValue.value + number
+}
+
+//Formula to make the decimal clickable once per calculation
+function displayDecimal (number) {
+    if (allowComa === true) {
+        InputValue.value = InputValue.value + number
+        allowComa = false;
+    }
+}
+function symbols (number) {
+    InputValue.value = InputValue.value + number;
+    allowComa = true;
 }
 function calculate() {
     var p =document.getElementById("input").value;
